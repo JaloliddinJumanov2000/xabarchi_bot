47d9a0acf3e88a598ffa4fd7ba5ff3329d7aada0
Jaloliddin <jumanovjaloliddin7@gmail.com>
2025-08-25 22:09:18 +0500

    new

--- root/urls.py
+++ root/urls.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from app import urls
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Xabarchi Bot",
@@ -18,6 +20,7 @@ def redirect_to_admin(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
+    path('', include('Xabarchi.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
