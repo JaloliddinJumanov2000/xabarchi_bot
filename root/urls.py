b42c891b18da2064196bf9f0ba14a5dc570fbd73
Jaloliddin <jumanovjaloliddin7@gmail.com>
2025-08-26 09:16:54 +0500

    new

--- root/urls.py
+++ root/urls.py
@@ -20,7 +20,6 @@ def redirect_to_admin(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
-    path('', include('Xabarchi.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
