c054f201fdc36c9a7984bdc7c8e0ddbd22eca107
Jaloliddin <jumanovjaloliddin7@gmail.com>
2025-09-06 21:52:45 +0500

    wsigi

--- root/wsgi.py
+++ root/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for root project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Django loyihangiz papkasini sys.path ga qo‘shish
+path = '/home/jaloliddin/xabarchi_bot'   # loyihangiz joylashgan to‘liq yo‘l
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'xabarchi_bot.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
-
 application = get_wsgi_application()
