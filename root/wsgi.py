a8150674c88dbe5f084b062da741c43abf4252e6
Jaloliddin <jumanovjaloliddin7@gmail.com>
2025-09-07 09:57:22 +0500

    wsgi

--- root/wsgi.py
+++ root/wsgi.py
@@ -12,3 +12,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'root.settings'
 # Django WSGI application yaratish
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+# Virtualenv yo‘lini sozlash
+activate_this = '/home/Jaloliddin/.virtualenvs/myvirtualenv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
