ff8d03fcd2b0cdf947c898b10ce35e356aafa790
Jaloliddin <jumanovjaloliddin7@gmail.com>
2025-11-01 15:13:37 +0500

    add domen

--- app/apps.py
+++ app/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class AppMainConfig(AppConfig):  # ⚠️ nomini o'zgartirdik
+class AppMainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
 
